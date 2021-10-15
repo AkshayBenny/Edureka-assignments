4db4560650140ae45478e0fb7af9e03a5b3035b4
AkshayBenny <akshaybennygh@gmail.com>
2021-10-15 17:15:43 +0530

    update

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const app = express();
 
-var items = [];
+let items = [];
+let workItems = [];
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
@@ -19,15 +21,29 @@ app.get("/", (req, res) => {
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newItem: items });
+  res.render("list", { listTitle: day, newListItems: items });
 });
 
 app.post("/", (req, res) => {
   var item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req, res) => {
+  let item = req.body.newItem;
   items.push(item);
-  res.redirect("/");
-  console.log(items);
-  console.log(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, () => {
